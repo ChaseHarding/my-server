956a4fed7baa055c1c2801954ba0e5911096d6ed
ChaseHarding <chaseharding965@gmail.com>
2024-01-30 21:36:37 -0800

    Succesful first trial with pletiful knowledge gained of status codes and npm functionality.

---
+++ index.js
@@ -0,0 +1,9 @@
+let http = require('http');
+let server = http.createServer((req, res) => {
+    res.write('<h1 style="color:blue">Hello FSD!</h1>')
+    res.end();
+})
+
+server.listen(3000, () => {
+    console.log('I am awake!')
+})
