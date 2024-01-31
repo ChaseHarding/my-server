6a329bd9665eece921812e6c4350dcbd5d5625f1
ChaseHarding <chaseharding965@gmail.com>
2024-01-30 21:44:58 -0800

    I didnt think learning status codes would be this amusing. Status code 418 is my new favorite. Im a teapot

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// let http = require('http');
+// let server = http.createServer((req, res) => {
+//     res.write('<h1 style="color:blue">Hello FSD!</h1>')
+//     res.end();
+// })
+
+// server.listen(3000, () => {
+//     console.log('I am awake!')
+// })
+
 let http = require('http');
 let server = http.createServer((req, res) => {
-    res.write('<h1 style="color:blue">Hello FSD!</h1>')
-    res.end();
+    res.writeHeader(418)
+    res.write('<h1>Meow!</h1><img src="https://http.cat/images/418.jpg" alt="418">')
+    res.end()
 })
 
 server.listen(3000, () => {
